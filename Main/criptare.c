38208215d75b0e3c38613a84b69f151c155210fb
Radu Teodor <raduteodorgeorge0@gmail.com>
2018-12-28 12:14:42 +0200

    Morning commit
    Nothing at all

--- Main/criptare.c
+++ Main/criptare.c
@@ -136,8 +136,8 @@ void genperm_array(unsigned long *perm,unsigned long *rng,int n)
 
 void switch_pixels(unsigned char *data,unsigned x,unsigned y,unsigned latime_img,unsigned padding)
 {
-    x=(x-1)*3+(x/latime_img)*padding+54;
-    y=(y-1)*3+(y/latime_img)*padding+54;
+    x=(x-1)*3+((x-1)/latime_img)*padding+54;
+    y=(y-1)*3+((y-1)/latime_img)*padding+54;
     unsigned char temp1 = data[x];
     unsigned char temp2 = data[x+1];
     unsigned char temp3 = data[x+2];
@@ -172,9 +172,7 @@ unsigned char *apply_perm(unsigned char *data,unsigned long int *perm,unsigned i
     for(int i=0;i<=n-1;i++)
     {
         unsigned long int j=perm[i];
-        transfer_pixels(data,data2,i+1,j+1,latime_img,0);
-       // printf("%lu schimbat cu %lu \n",i+1,j+1);
-
+        transfer_pixels(data,data2,i+1,j+1,latime_img,latime_img%4);
     }
     return data2;
 }
@@ -210,6 +208,9 @@ int main()
     //switch_pixels(data,1,2,latime_img,1);
 
     data = apply_perm(data,perm,inaltime_img*latime_img,latime_img);
+
+
+
     reverse_array(data,header);
     WriteBMP(nume_img_criptata,data);
 
